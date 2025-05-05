e9e6e07f9ab001be9f1f7c2278119fda49d1f28a
harleychewy <lopeza4869@gmail.com>
2025-05-05 11:04:36 -0700

    Initial Commit

---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Student1 on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
